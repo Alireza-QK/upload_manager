1ab164d03aed31e682bb0b2eac66004f718818df
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-27 12:09:46 +0430

    added app imagekit

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # External apps
+    'imagekit',
+
     # Our apps
     'manager.apps.ManagerConfig',
 ]
