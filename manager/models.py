84fdc502c2483535ec2125db86f55389d5f61903
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-27 12:14:58 +0430

    init model UploadManager

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,16 @@
+import uuid
+import os
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
 
-# Create your models here.
+from .utils import *
+
+class UploadManager(models.Model):
+    """
+		Represents a uploader image file
+	"""
+
+    pass
