d7a0f9530b86c84d890854ddf25404ad57e83715
Alireza-QK <Alireza.QK.75@gmail.com>
2021-05-27 22:51:18 +0430

    add url delete image to urls.py

--- manager/urls.py
+++ manager/urls.py
@@ -6,4 +6,5 @@ app_name = 'manager'
 urlpatterns = [
     path('', ImageListView.as_view(), name="list_image"),
     path('upload/', CreateUploadView.as_view(), name="upload"),
+    path('delete/<int:pk>', DeleteUploadView.as_view(), name="delete"),
 ]
